cbee958bee80b802c346a67b210dba00200bdcd8
DucNguyen <minhducnguyen189@gmail.com>
2022-09-24 21:26:18 +0700

    test java servlets

---
+++ java-servlets/src/main/java/com/java/servlets/SampleServlet.java
@@ -0,0 +1,22 @@
+package com.java.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class SampleServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        Date date = new Date();
+        out.println("<html> " +
+                "<body>" +
+                "<h1 align=center>HF\'s SampleServlet</h1>" +
+                "<br>" + date + "</body>" + "</html>");
+    }
+
+}
